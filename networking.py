12f14e8bf09457be0777a24266a81e23af3a9cf4
Stephen Axtell <axtell.stephen@gmail.com>
2020-09-06 10:39:10 +0200

    basics of networking

---
+++ networking.py
@@ -0,0 +1,37 @@
+# import socket
+
+# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# mysock.connect(('data.pr4e.org', 80))
+# cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+# mysock.send(cmd)
+
+# while True:
+#     data = mysock.recv(512)
+#     if (len(data) < 1):
+#         break
+#     print(data.decode())
+
+# mysock.close()
+
+# instead of sockets one can use urllib
+
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
+
+# processing html with beautiful soup
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# retrieve anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+print(ord('S'))  # 83

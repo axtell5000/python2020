096f34789465af4881fbc87c42135f545ce41bdf
Stephen Axtell <axtell.stephen@gmail.com>
2020-09-08 09:53:30 +0200

    working with objects in Python

---
+++ party6.py
@@ -0,0 +1,20 @@
+from party import PartyAnimal
+
+# inheriting everything from PartyAnimal
+
+
+class CricketFan(PartyAnimal):
+    points = 0
+
+    def six(self):
+        self.points = self.points + 6
+        self.party()
+        print(self.name, "points", self.points)
+
+
+s = PartyAnimal("Sally")
+s.party()
+j = CricketFan("Jim")
+j.party()
+j.six()
+print(dir(j))
